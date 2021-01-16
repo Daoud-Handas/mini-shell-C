714cac58fafeefdc436031226dbb9adb4b5ddbba
Oniizukaa <daoudhandas@gmail.com>
2021-01-16 19:47:30 +0100

    commentaires ajoutes

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
-//#define _GNU_SOURCE // GNU_SOURCE pour avoir getline qui permet de borner la lecture de ligne
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h> //fork, pid_t
-#include <sys/wait.h> //wait du parent
+#include <sys/wait.h>  //wait
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>    //strstr
+
+/*
+
+                MINI SHELL - LANGAGE C
+                   2ESGIA2 2020/2021
+                       Groupe :
+
+   Romain LETHUMIER  Daoud HANDAS  Hamza ID LAHCEN
+      “ヽ(´▽｀)ノ”       (•̀ᴗ•́)و ̑̑       (っ▀¯▀)つ
+
+
+*/
+
 
 int main(int argc, char * argv[]) {
 
@@ -13,7 +25,7 @@ int main(int argc, char * argv[]) {
     char * variable[2] = {argv[1], NULL};
     char * buffer = NULL;
 	size_t	buf_size = 2048;
-	char **cmd = NULL;
+	char * pipe [1] = {'|'};
 
         //fork
         pid_t pid = fork();
@@ -24,27 +36,32 @@ int main(int argc, char * argv[]) {
             return EXIT_FAILURE;
 
         } else if (pid > 0)
-        {
-            // On est dans le père
-            wait(&status);
-            perror("wait père");
-            while (getline(&buffer, &buf_size, stdin) > 0)
-            {
-                //Pipeline
-                if (strstr(buffer, '|')!= NULL)
+            {// On est dans le père
+
+                wait(&status);
+                perror("wait père");
+
+                //Tant que l'user saisis
+                while (getline(&buffer, &buf_size, stdin) > 0)
                 {
-                    cmd = strtok(buffer, '|');
+
+                    //Execution de la commande
+                    system(buffer);
+
+                    //Status de la commande
+                    if (strstr(buffer, pipe))
+                        perror("Multi commandes");
+                    else
+                        perror("Commande");
+
+                    //Prompt
+                    write(1, "Daoud@root> ", 12);
                 }
 
+            } else {// On est dans le fils
 
-                system(cmd);
-                write(1, "Daoud@root> ", 12);
-            }
-
-        } else {
-            // On est dans le fils
-            perror("fork fils");
-            execvp(programName,variable);
+                perror("fork fils");
+                execvp(programName,variable);
 
         }
     printf("Fin du Shell");
